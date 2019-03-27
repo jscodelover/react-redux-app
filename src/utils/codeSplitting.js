de9f93c0b02065fc37307977ea2d5b1170aea85f
jscodelover <manisha.4918@gmail.com>
2019-03-27 23:40:16 +0530

    feat(components): create a folder that contain global resuable comp

--- src/utils/codeSplitting.js
+++ src/utils/codeSplitting.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Loadable from 'react-loadable';
+import { Loading } from '../components';
 
 export default function createSplitPoint(path) {
   return Loadable({
     loader: () => import(`../modules/${path}`),
-    loading: () => <h3>Lodaing...</h3>,
+    loading: () => <Loading />,
   });
 }
